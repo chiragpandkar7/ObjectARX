c7fe3216ba0aaf41c245bba68a02eee9cd24831f
chiragpandkar7 <149322622+chiragpandkar7@users.noreply.github.com>
2023-11-27 12:43:15 +0530

    Add files via upload

---
+++ Project9/Assignment9/Project9/Project9/Project9.h
@@ -0,0 +1,32 @@
+// Project9.h : main header file for the Project9 DLL
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+// Load the common Windows headers
+#include <windows.h>
+// Load the common AutoCAD headers
+#include "arxHeaders.h"
+#include "dbents.h"
+
+
+// CProject9App
+// See Project9.cpp for the implementation of this class
+//
+
+class CProject9App : public CWinApp
+{
+public:
+	CProject9App();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+	static void changeColor();
+	DECLARE_MESSAGE_MAP()
+};
